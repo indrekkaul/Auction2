0a7fcbbab5d2eefaa0df2ef7df0dd2a46f65b4a1
indrekkaul <jadmadmoja7>
2021-05-15 21:22:49 +0300

    UserAccount class, replaced geters seter whit @Data anotations, and add @Entity @Table and @Id etc annotations for database

--- src/main/java/com/example/auction/model/UserAccount.java
+++ src/main/java/com/example/auction/model/UserAccount.java
@@ -1,68 +1,33 @@
 package com.example.auction.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.Date;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table
+@Table(name = "USER_ACCOUNT")
 public class UserAccount {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int userId;
+    private Long userId;
+    @Column(name = "account_name")
     private String accountName;
+    @Column(name = "email") // Also User login name!
     private String email;
+    @Column(name = "password")
     private String password;
+
     private Address address;
+    @Column(name = "account_name")
     private Date accountCreated;
     private enum accountStatus{ACTIVE, INACTIVE, BLOCKED}
     private enum accountType{STANDARD, PREMIUM}
 
-    public int getUserId() {
-        return userId;
-    }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getAccountName() {
-        return accountName;
-    }
-
-    public void setAccountName(String accountName) {
-        this.accountName = accountName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public Date getAccountCreated() {
-        return accountCreated;
-    }
-
-    public void setAccountCreated(Date accountCreated) {
-        this.accountCreated = accountCreated;
-    }
 }
