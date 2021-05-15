de6895090bf52d9aa2ac481c37226234ce8a7066
traanels <tanel.sempelson.001@gmail.com>
2021-05-15 15:52:29 +0300

    added user account and address classes

---
+++ src/main/java/com/example/auction/model/Address.java
@@ -0,0 +1,52 @@
+package com.example.auction.model;
+
+public class Address {
+
+    private String street;
+    private int postalCode;
+    private String city;
+    private String state;
+    private String country;
+
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
