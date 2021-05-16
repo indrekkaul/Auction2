de8c667d8a03b657116102ed599147d293ad1639
indrekkaul <indrek.kaul@gmail.com>
2021-05-16 12:13:34 +0300

    added AccountTypeService

---
+++ src/main/java/com/example/auction/services/AccountTypeService.java
@@ -0,0 +1,10 @@
+package com.example.auction.services;
+
+import com.example.auction.model.AccountType;
+
+import java.util.List;
+
+public interface AccountTypeService {
+    List<AccountType> findAll();
+
+}
