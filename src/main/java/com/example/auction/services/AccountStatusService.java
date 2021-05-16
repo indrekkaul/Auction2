d3f8c6754eeb49e7e19004cc164db788bb777a52
indrekkaul <indrek.kaul@gmail.com>
2021-05-16 12:10:09 +0300

    added AccountStatusService

---
+++ src/main/java/com/example/auction/services/AccountStatusService.java
@@ -0,0 +1,12 @@
+package com.example.auction.services;
+
+import com.example.auction.model.AccountStatus;
+
+import java.util.List;
+
+public interface AccountStatusService {
+
+    List<AccountStatus> findAll();
+
+
+}
