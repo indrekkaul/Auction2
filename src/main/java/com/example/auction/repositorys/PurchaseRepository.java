d9fe1bfe2924666b412e06a46cac9f275386850d
indrekkaul <indrek.kaul@gmail.com>
2021-05-16 11:36:42 +0300

    Purchase repository

--- src/main/java/com/example/auction/repositorys/PurchaseRepository.java
+++ src/main/java/com/example/auction/repositorys/PurchaseRepository.java
@@ -1,9 +1,10 @@
 package com.example.auction.repositorys;
 
+import com.example.auction.model.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PurchaseRepository extends JpaRepository<PurchaseRepository, Long> {
+public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
 
 }
