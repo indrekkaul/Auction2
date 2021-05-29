48d8d4bf5e833e24ef2362e3583c232547a81ce2
Hans <kristjanladva@gmail.com>
2021-05-29 04:17:52 +0300

    Add auction component, directory restructuring

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AuctionComponent } from './components/auction/auction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AuctionComponent
   ],
   imports: [
     BrowserModule,
